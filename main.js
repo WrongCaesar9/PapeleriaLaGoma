662c911c419d21dc39839ac77ad5a771ab7ba4dd
DESKTOP-AD6CQ3G\josue <josuejuarez12396@gmail.com>
2023-05-15 18:46:56 -0600

    Se agrego un botón de sroll, nuevos artículos, se cambiaron algunos valores de los colores y se organizo mejor el documento html

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const productosContenedor = document.getElementById("productos-contenedor");
 
-// Obtener los datos de las bicicletas
+// Obtener los datos de los productos
 fetch("data.json")
     .then(response => response.json())
     .then(data => {
         // Almacenar los datos en una variable global
         window.productos = data.productos;
-        // Mostrar las bicicletas en la página
+        // Mostrar los productos en la página
         mostrarProductos();
     })
     .catch(error => console.error(error));
 
-// Función para mostrar las bicicletas en la página
+// Función para mostrar los productos en la página
 function mostrarProductos() {
     const productosContenedor = document.getElementById("productos-contenedor");
     productosContenedor.innerHTML = "";
@@ -22,31 +22,31 @@ function mostrarProductos() {
 
     console.log(filtroPrecio);
 
-    // Recorrer cada bicicleta
+    // Recorrer cada producto en la papeleria
     window.productos.forEach(function (productos) {
-        // Comprobar si la bicicleta cumple con los criterios de los filtros
+        // Comprobar si los articulos cumplen con los criterios de los filtros
         if ((filtroModelo === "" || productos.modelo === filtroModelo) && (filtroPrecio === 0 || productos.precio <= filtroPrecio)
         ) {
-            // Crear un elemento div para la bicicleta
+            // Crear un elemento div para el articulo
             const productosDiv = document.createElement("div");
             productosDiv.classList.add("productos");
-            // Crear una imagen para la bicicleta
+            // Crear una imagen para el articulo
             const productosImg = document.createElement("img");
             productosImg.src = productos.img;
             productosImg.alt = productos.modelo;
             productosDiv.appendChild(productosImg);
 
-            // Crear un h3 para el nombre de la bicicleta
+            // Crear un h3 para el nombre del producto
             const productosNombre = document.createElement("h3");
             productosNombre.innerHTML = productos.nombre;
             productosDiv.appendChild(productosNombre);
 
-            // Crear un p para el modelo de la bicicleta
+            // Crear un p para el modelo del producto
             const productosModel = document.createElement("p");
             productosModel.innerHTML = productos.modelo;
             productosDiv.appendChild(productosModel);
 
-            // Crear un p para el precio de la bicicleta
+            // Crear un p para el precio del producto
             const productosPrice = document.createElement("p");
             productosPrice.innerHTML = "$"+productos.precio;            
             productosDiv.appendChild(productosPrice);
@@ -56,7 +56,24 @@ function mostrarProductos() {
         }
     });
 }
-
-// Agregar eventos a los filtros para que al cambiar su valor, se vuelva a mostrar las bicicletas
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
+    }
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+// Agregar eventos a los filtros para que al cambiar su valor, se vuelva a mostrar los articulos
 document.getElementById("filtro-modelo").addEventListener("change", mostrarProductos);
 document.getElementById("filtro-precio").addEventListener("change", mostrarProductos);
+
