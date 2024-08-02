592fc808175d38cb06e5591d3e96a1d52c39cc0e
DESKTOP-AD6CQ3G\josue <josuejuarez12396@gmail.com>
2024-08-02 17:53:22 -0600

    Esto no se xd

--- BallRoomWeb/scripts.js
+++ BallRoomWeb/scripts.js
@@ -273,3 +273,82 @@ window.addEventListener('resize', function () {
     frame.style.width = `${frameWidth}px`;
   });
 });
+
+
+
+
+
+
+
+/* global woodmart_settings */
+(function($) {
+	woodmartThemeModule.scrollTop = function() {
+		var $scrollTop = $('.scrollToTop');
+
+		woodmartThemeModule.$window.on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$scrollTop.addClass('button-show');
+			} else {
+				$scrollTop.removeClass('button-show');
+			}
+		});
+
+		$scrollTop.on('click', function() {
+			$('html, body').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	};
+
+	$(document).ready(function() {
+		woodmartThemeModule.scrollTop();
+	});
+})(jQuery);
+
+/* global woodmart_settings */
+(function($) {
+	woodmartThemeModule.$document.on('wdShopPageInit', function() {
+		woodmartThemeModule.widgetCollapse();
+	});
+
+	woodmartThemeModule.$window.on('resize', woodmartThemeModule.debounce(function() {
+		woodmartThemeModule.widgetCollapse();
+	}, 300));
+
+	woodmartThemeModule.widgetCollapse = function() {
+		var $footer = $('.main-footer .footer-widget');
+
+		if ('yes' === woodmart_settings.collapse_footer_widgets && 0 < $footer.length) {
+			if (woodmartThemeModule.$window.innerWidth() <= 575) {
+				$footer.addClass('wd-widget-collapse');
+			} else {
+				$footer.removeClass('wd-widget-collapse');
+				$footer.find('> *:not(.widget-title)').show();
+			}
+		}
+
+		$('.wd-widget-collapse .widget-title').off('click').on('click', function() {
+			var $title = $(this);
+			var $widget = $title.parent();
+			var $content = $widget.find('> *:not(.widget-title)');
+
+			if ($widget.hasClass('wd-opened')) {
+				$widget.removeClass('wd-opened');
+				$content.stop().slideUp(200);
+			} else {
+				$widget.addClass('wd-opened');
+				$content.stop().slideDown(200);
+				woodmartThemeModule.$document.trigger('wood-images-loaded');
+			}
+		});
+	};
+
+	$(document).ready(function() {
+		woodmartThemeModule.widgetCollapse();
+	});
+
+	window.addEventListener('popstate', function() {
+		woodmartThemeModule.widgetCollapse();
+	});
+})(jQuery);
