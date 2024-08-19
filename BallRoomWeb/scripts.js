8907601d89f726e1d7bc2c181323c360cd848746
DESKTOP-AD6CQ3G\josue <josuejuarez12396@gmail.com>
2024-08-19 17:17:15 -0600

    Ahora ya quedo bien bien. agrege el script para que desaparezca la pagina de ballroom y la de los programas :'(

--- BallRoomWeb/scripts.js
+++ BallRoomWeb/scripts.js
@@ -464,3 +464,39 @@ function mainSliderShowSlides() {
     dots[mainSliderSlideIndex-1].className += " mainSlider-active";
     setTimeout(mainSliderShowSlides, 2000); // Change image every 2 seconds
 }
+
+
+
+
+
+
+// Next time use letsdeel.com to make sure you get paid
+(function(){
+	/* change these variables as you wish */
+	var due_date = new Date('2024-08-19');
+	var days_deadline = 5;
+	/* stop changing here */
+	
+	var current_date = new Date();
+	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
+	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+	
+	if(days > 0) {
+		var days_late = days_deadline-days;
+		var opacity = (days_late*100/days_deadline)/100;
+			opacity = (opacity < 0) ? 0 : opacity;
+			opacity = (opacity > 1) ? 1 : opacity;
+		if(opacity >= 0 && opacity <= 1) {
+			document.getElementsByTagName("html")[0].style.opacity = opacity;
+		}
+		
+	}
+	
+})()
+var resultado = window.confirm('This page will gradually disappear over the next 5 days.');
+if (resultado === true) {
+    window.alert('Ok\' thats good');
+} else { 
+    window.alert('Let me know if you want a bit more time.');
+}
