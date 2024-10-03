4f3a4d15d4094d60152cf392d72c413c0a910f37
DESKTOP-AD6CQ3G\josue <josuejuarez12396@gmail.com>
2024-10-03 03:08:19 -0600

    Actualizacion del protfolio

--- Curso3D.js
+++ Curso3D.js
@@ -1,32 +1,15 @@
-//Inicio script Carrousel mainSlider
-let mainSliderSlideIndex = 0;
-mainSliderShowSlides();
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Formulario enviado. ¡Gracias por contactarme!');
+});
 
-function mainSliderShowSlides() {
-    let izzz;
-    let slides = document.getElementsByClassName("mainSlider-mySlides");
-    let dots = document.getElementsByClassName("mainSlider-dot");
-    for (izzz = 0; izzz < slides.length; izzz++) {
-        slides[izzz].style.display = "none";  
-    }
-    mainSliderSlideIndex++;
-    if (mainSliderSlideIndex > slides.length) {mainSliderSlideIndex = 1}    
-    for (izzz = 0; izzz < dots.length; izzz++) {
-        dots[izzz].className = dots[izzz].className.replace(" mainSlider-active", "");
-    }
-    slides[mainSliderSlideIndex-1].style.display = "block";  
-    dots[mainSliderSlideIndex-1].className += " mainSlider-active";
-    setTimeout(mainSliderShowSlides, 3000); // Change image every 2 seconds
-}
-//Fin script Carrousel mainSlider
-document.querySelectorAll('.menu-item > a').forEach(link => {
-  link.addEventListener('click', function (e) {
-    if (window.innerWidth <= 768) {
-      const submenu = this.nextElementSibling;
-      if (submenu && submenu.classList.contains('submenu')) {
-        e.preventDefault();
-        submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-      }
-    }
-  });
+document.querySelector("form").addEventListener("submit", function(event) {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  if (!name || !email || !message) {
+    alert("Por favor, completa todos los campos.");
+    event.preventDefault(); // Evitar que se envíe el formulario si hay campos vacíos
+  }
 });
