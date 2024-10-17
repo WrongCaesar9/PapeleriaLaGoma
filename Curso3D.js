f972540da2d92efb5e9928ced829337650b476dd
DESKTOP-AD6CQ3G\josue <josuejuarez12396@gmail.com>
2024-10-16 22:19:41 -0600

    Se ajusto el js para hacer qu se cierre el menu al seleccionanr un submenu y se arreglo la posicion de lahamburgesa

--- Curso3D.js
+++ Curso3D.js
@@ -1,3 +1,19 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-  document.querySelector('.sidebar').classList.toggle('active');
+document.addEventListener("DOMContentLoaded", function() {
+  // Obtener el menú lateral y los enlaces
+  const sidebar = document.querySelector('.sidebar');
+  const links = document.querySelectorAll('.sidebar nav a');
+
+  // Añadir un evento de clic a cada enlace
+  links.forEach(link => {
+    link.addEventListener('click', function() {
+      // Cerrar el menú lateral
+      sidebar.classList.remove('active');
+    });
+  });
+
+  // Código para el menú hamburguesa
+  const hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', function() {
+    sidebar.classList.toggle('active');
+  });
 });
