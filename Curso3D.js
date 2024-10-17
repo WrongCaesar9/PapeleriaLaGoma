9a5342d2a2d2a249c83e74d81c9320b3600762eb
DESKTOP-AD6CQ3G\josue <josuejuarez12396@gmail.com>
2024-10-16 22:04:36 -0600

    Ahora supongo que ya fubnciona bien, se cambio el css y html y ademas se agrego el menu responsive

--- Curso3D.js
+++ Curso3D.js
@@ -1,15 +1,3 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Formulario enviado. ¡Gracias por contactarme!');
-});
-
-document.querySelector("form").addEventListener("submit", function(event) {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  if (!name || !email || !message) {
-    alert("Por favor, completa todos los campos.");
-    event.preventDefault(); // Evitar que se envíe el formulario si hay campos vacíos
-  }
+document.getElementById('menu-toggle').addEventListener('click', function() {
+  document.querySelector('.sidebar').classList.toggle('active');
 });
